273a7a206bcfa7d2358d1cb87beb5b1c4b42401d
Kobi Hari <kobi2294@yahoo.com>
2023-06-17 16:28:58 +0300

    added the quizes feature route

--- ngrx-quiz-box/src/app/app.routes.ts
+++ ngrx-quiz-box/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'quizes', pathMatch: 'full'},
+    { 
+        path: 'quizes', 
+        loadComponent: () => import('./features/quizes/quizes-list/quizes-list.component').then(m => m.QuizesListComponent)
+    }
+];
---
+++ ngrx-quiz-box/src/app/features/quizes/quizes-list/quizes-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quizes-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quizes-list.component.html',
+  styleUrls: ['./quizes-list.component.scss']
+})
+export class QuizesListComponent {
+
+}
