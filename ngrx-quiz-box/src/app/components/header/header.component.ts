3e5e961f991979906554e756642500f9466b9abd
Kobi Hari <kobi2294@yahoo.com>
2023-06-19 01:07:21 +0300

    added the uid package

--- ngrx-quiz-box/src/app/components/header/header.component.ts
+++ ngrx-quiz-box/src/app/components/header/header.component.ts
@@ -5,7 +5,8 @@ import { NavItemDirective } from './nav-item.directive';
 import { RouterModule } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { AddQuizDialogComponent } from '../add-quiz-dialog/add-quiz-dialog.component';
-import { RunQuizDialogComponent } from '../run-quiz-dialog/run-quiz-dialog.component';
+import { v4 } from 'uuid';
+
 
 @Component({
   selector: 'app-header',
@@ -18,6 +19,9 @@ export class HeaderComponent {
   constructor(private matDialog: MatDialog){}
 
   open() {
+    const uid = v4();
+    console.log(uid);
+
     this.matDialog.open(AddQuizDialogComponent, {
       disableClose: true, 
       hasBackdrop: false
